152f85386de78487e61c1c30074c22ba352ff8ae
giovannitangredi <giovanni.tangredi1@libero.it>
2022-02-01 19:25:01 +0100

    Added unit test and pinned dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use rust_code_analysis::{dump_root, metrics, read_file, ParserTrait, RustParser};
+use rust_code_analysis::{metrics, read_file, ParserTrait, RustParser};
 use serde_json::*;
 use std::collections::*;
 use std::fs;
 use std::path::PathBuf;
 
-fn sifis_plain(path: PathBuf, covs: Vec<Value>) {
+fn sifis_plain(path: &PathBuf, covs: Vec<Value>) -> f64 {
     let data = read_file(&path).unwrap();
     let parser = RustParser::new(data, &path, None);
     let space = metrics(&parser, &path).unwrap();
@@ -22,18 +22,14 @@ fn sifis_plain(path: PathBuf, covs: Vec<Value>) {
         }
     }
     let sifis = sum / ploc;
-    println!(
-        "For {:?} the SIFIS plain value is {:?}",
-        path.file_name().unwrap(),
-        sifis
-    );
+    sifis
 }
-fn read_json(file: String) -> HashMap<String, Vec<Value>> {
+fn read_json(file: String, prefix: &str) -> HashMap<String, Vec<Value>> {
     let val: Value = serde_json::from_str(file.as_str()).unwrap();
     let vec = val["source_files"].as_array().unwrap();
     let mut covs = HashMap::<String, Vec<Value>>::new();
     for x in vec {
-        let mut name = "../rust-data-structures-main/".to_string();
+        let mut name = prefix.to_string();
         name = name + x["name"].as_str().unwrap();
         covs.insert(name.to_string(), x["coverage"].as_array().unwrap().to_vec());
     }
@@ -42,11 +38,49 @@ fn read_json(file: String) -> HashMap<String, Vec<Value>> {
 fn main() {
     let paths = fs::read_dir("../rust-data-structures-main/src").unwrap();
     let file = fs::read_to_string("./src/coveralls.json").unwrap();
-    let covs = read_json(file);
+    let covs = read_json(file,"../rust-data-structures-main/");
     for path in paths {
         let p = path.unwrap().path();
         let key =p.to_str().unwrap().to_string();
         let arr = covs.get(&key).unwrap().to_vec();
-        sifis_plain(p, arr)
+        let sifis =sifis_plain(&p, arr);
+        println!(
+            "For {:?} the SIFIS plain value is {:?}",
+            p.file_name().unwrap(),
+            sifis
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn test_read_json(){
+    let file = fs::read_to_string("./test/test.json").unwrap();
+    let covs = read_json(file,"../rust-data-structures-main/");
+    assert_eq!(covs.contains_key("../rust-data-structures-main/test/test.rs"), true);
+    assert_eq!(covs.contains_key("../rust-data-structures-main/src/main.rs"), true);
+    let vec= covs.get("../rust-data-structures-main/test/test.rs").unwrap();
+    assert_eq!(vec.len(),12);
+    let vec_main= covs.get("../rust-data-structures-main/src/main.rs").unwrap();
+    assert_eq!(vec_main.len(),9);
+    let  value = vec.get(6).unwrap();
+    assert_eq!(value,2);
+    let  value_null = vec.get(1).unwrap();
+    assert_eq!(value_null.is_null(),true);
+    }
+    
+    #[test]
+    fn test_sifis_plain(){
+    let file = fs::read_to_string("./test/test.json").unwrap();
+    let covs = read_json(file,"../rust-data-structures-main/");   
+    let mut path = PathBuf::new();
+    path.push("./test/test.rs");
+    let sifis = sifis_plain(&path,covs.get("../rust-data-structures-main/test/test.rs").unwrap().to_vec());
+    assert_eq!(sifis,24./10.)
     }
 }
---
+++ test/test.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //simple test
+    let a=3;
+    let mut b=4;
+    if a==3{
+        if b%2
+        {
+            b--;
+        }
+    }
+    //comment not count
+}
