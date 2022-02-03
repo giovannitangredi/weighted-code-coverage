e34c999ce307f862bbbb706e9b44b1602d90a6ea
giovannitangredi <giovanni.tangredi1@libero.it>
2022-02-03 12:22:10 +0100

    Add sifis quantized version and refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use rust_code_analysis::{metrics, read_file, ParserTrait, RustParser};
+use rust_code_analysis::{metrics, read_file, ParserTrait, RustParser, FuncSpace};
 use serde_json::*;
 use std::collections::*;
 use std::fs;
 use std::path::PathBuf;
 
-fn sifis_plain(path: &PathBuf, covs: Vec<Value>) -> f64 {
-    let data = read_file(&path).unwrap();
-    let parser = RustParser::new(data, &path, None);
-    let space = metrics(&parser, &path).unwrap();
+fn sifis_plain(path: &PathBuf, covs: &Vec<Value>) -> f64 {
+    let data = read_file(path).unwrap();
+    let parser = RustParser::new(data, path, None);
+    let space = metrics(&parser, path).unwrap();
     let ploc = space.metrics.loc.ploc();
     let comp = space.metrics.cyclomatic.cyclomatic_sum();
     let mut sum = 0.0;
+
     for i in 0..covs.len() {
         let is_null = covs.get(i).unwrap().is_null();
 
@@ -21,19 +22,62 @@ fn sifis_plain(path: &PathBuf, covs: Vec<Value>) -> f64 {
             }
         }
     }
-    let sifis = sum / ploc;
-    sifis
+    sum / ploc
+   
 }
+
 fn read_json(file: String, prefix: &str) -> HashMap<String, Vec<Value>> {
     let val: Value = serde_json::from_str(file.as_str()).unwrap();
     let vec = val["source_files"].as_array().unwrap();
     let mut covs = HashMap::<String, Vec<Value>>::new();
     for x in vec {
         let mut name = prefix.to_string();
-        name = name + x["name"].as_str().unwrap();
+        name += x["name"].as_str().unwrap();
         covs.insert(name.to_string(), x["coverage"].as_array().unwrap().to_vec());
     }
     covs
+}
+
+fn sifis_quantized(path: &PathBuf, covs: &Vec<Value>) -> f64 {
+    let data = read_file(path).unwrap();
+    let parser = RustParser::new(data, path, None);
+    let root = metrics(&parser, path).unwrap();
+    let ploc = root.metrics.loc.ploc();
+    let mut sum = 0.0;
+    let mut spaces : Vec<FuncSpace> = Vec::<FuncSpace>::new();
+    let mut stack : Vec<FuncSpace> = Vec::<FuncSpace>::new();
+    stack.push(root.clone());
+    spaces.push(root);
+    // get all spaces
+    while let Some(space) = stack.pop() {
+        for s in space.spaces.into_iter()
+        {
+            stack.push(s.clone()); 
+            spaces.push(s.clone())
+        }
+    }
+    //for each line find the minimun space and the add complexity to sum
+    for i in 0..covs.len() {
+        let is_null = covs.get(i).unwrap().is_null();
+
+        if !is_null {
+            let cov = covs.get(i).unwrap().as_u64().unwrap();
+            if cov > 0 {
+               let mut min = usize::MAX;
+               let mut min_space : FuncSpace = spaces[0].clone();
+               for j in &spaces {
+                   if i >= j.start_line && i<= j.end_line && min >= (j.end_line - j.start_line) {
+                       min = j.end_line - j.start_line;
+                       min_space = j.clone();
+                   }
+
+               }
+               let comp = min_space.metrics.cyclomatic.cyclomatic();
+               sum+=comp;
+            }
+        }
+    } 
+    sum / ploc
 }
 fn main() {
     let paths = fs::read_dir("../rust-data-structures-main/src").unwrap();
@@ -43,11 +87,17 @@ fn main() {
         let p = path.unwrap().path();
         let key =p.to_str().unwrap().to_string();
         let arr = covs.get(&key).unwrap().to_vec();
-        let sifis =sifis_plain(&p, arr);
+        let sifis =sifis_plain(&p, &arr);
         println!(
-            "For {:?} the SIFIS plain value is {:?}",
+            "For {:?} the SIFIS plain value is {:.3?}",
             p.file_name().unwrap(),
             sifis
+        );
+        let sifis_quantized = sifis_quantized(&p,&arr);
+        println!(
+            "For {:?} the SIFIS quantized value is {:.3?}",
+            p.file_name().unwrap(),
+            sifis_quantized
         );
     }
 }
@@ -80,7 +130,22 @@ mod tests {
     let covs = read_json(file,"../rust-data-structures-main/");   
     let mut path = PathBuf::new();
     path.push("./test/test.rs");
-    let sifis = sifis_plain(&path,covs.get("../rust-data-structures-main/test/test.rs").unwrap().to_vec());
+    let vec =  covs.get("../rust-data-structures-main/test/test.rs").unwrap().to_vec();
+    let sifis = sifis_plain(&path,&vec);
     assert_eq!(sifis,24./10.)
     }
+
+    #[test]
+    fn test_sifis_quantized(){
+    let file = fs::read_to_string("./test/test.json").unwrap();
+    let covs = read_json(file,"../rust-data-structures-main/");   
+    let mut path = PathBuf::new();
+    path.push("./test/test.rs");
+    let vec =  covs.get("../rust-data-structures-main/test/test.rs").unwrap().to_vec();
+    let sifis = sifis_quantized(&path,&vec);
+    assert_eq!(sifis,18./10.)
+    }
 }
+
+
+
--- test/test.rs
+++ test/test.rs
@@ -2,7 +2,7 @@ fn main() {
     //simple test
     let a=3;
     let mut b=4;
-    if a==3{
+    if a==3 {
         if b%2
         {
             b--;
