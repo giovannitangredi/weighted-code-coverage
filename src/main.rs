0716e1646d85a31d301f1a46cf5ac9cb6d6080cc
giovannitangredi <giovanni.tangredi1@libero.it>
2022-03-01 15:18:02 +0100

    Added Crap and SkunkScore basic implementaions, Refactored some code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 mod sifis;
-use std::path::PathBuf;
-use crate::sifis::{get_sifis, SifisError};
+use crate::sifis::{get_metrics, SifisError};
 use clap::Parser;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Path to the project folder
-    #[clap(short='p', long="path_file", parse(from_os_str), value_name = "FILE")]
+    #[clap(
+        short = 'p',
+        long = "path_file",
+        parse(from_os_str),
+        value_name = "FILE"
+    )]
     path_file: PathBuf,
 
     /// path to the grcov json
-    #[clap(short='j', long="path_json", parse(from_os_str), value_name = "FILE")]
+    #[clap(
+        short = 'j',
+        long = "path_json",
+        parse(from_os_str),
+        value_name = "FILE"
+    )]
     path_json: PathBuf,
 }
 
 fn main() -> Result<(), SifisError> {
     let args = Args::parse();
-    match get_sifis(&args.path_file, &args.path_json) {
+    match get_metrics(&args.path_file, &args.path_json) {
         Ok(()) => Ok(()),
-        Err(err) =>  Err(err)
+        Err(err) => Err(err),
     }
 }
--- src/sifis.rs
+++ src/sifis.rs
@@ -21,27 +21,22 @@ pub enum SifisError {
 
 ///This function read all  the files in the project folder
 /// Returns all the Rust files, ignoring the other files or an error in case of problems
-fn read_files(files_path : &Path) -> Result<Vec<String>,SifisError> {
+fn read_files(files_path: &Path) -> Result<Vec<String>, SifisError> {
     let mut vec = vec![];
     let mut first = PathBuf::new();
     first.push(files_path);
-    let mut stack  = vec![first];
+    let mut stack = vec![first];
     while let Some(path) = stack.pop() {
         if path.is_dir() {
             let paths = match fs::read_dir(path.clone()) {
                 Ok(paths) => paths,
-                Err(_err) => {
-                    return Err(SifisError::WrongFile(
-                        path.display().to_string(),
-                    ))
-                }
+                Err(_err) => return Err(SifisError::WrongFile(path.display().to_string())),
             };
-    
+
             for p in paths {
                 stack.push(p.unwrap().path());
             }
-        }
-        else {
+        } else {
             let ext = path.extension();
 
             if ext != None && ext.unwrap() == "rs" {
@@ -50,7 +45,6 @@ fn read_files(files_path : &Path) -> Result<Vec<String>,SifisError> {
         }
     }
     Ok(vec)
-   
 }
 /// This fuction read the content of the coveralls  json file obtain by using grcov
 /// Return a HashMap with all the files arrays of covered lines using the path to the file as key
@@ -94,7 +88,7 @@ fn get_min_space(root: &FuncSpace, i: usize) -> FuncSpace {
 }
 
 /// Calculate the SIFIS plain value  for the given file(only rust language)
-/// Retrurn the value in case of success and an specif error in case of fails
+/// Return the value in case of success and an specif error in case of fails
 fn sifis_plain(path: &Path, covs: &[Value]) -> Result<f64, SifisError> {
     let data = match read_file(path) {
         Ok(data) => data,
@@ -129,7 +123,7 @@ fn sifis_plain(path: &Path, covs: &[Value]) -> Result<f64, SifisError> {
 }
 
 /// Calculate the SIFIS quantized value  for the given file(only rust language)
-/// Retrurn the value in case of success and an specif error in case of fails
+/// Return the value in case of success and an specif error in case of fails
 fn sifis_quantized(path: &Path, covs: &[Value]) -> Result<f64, SifisError> {
     let data = match read_file(path) {
         Ok(data) => data,
@@ -169,12 +163,79 @@ fn sifis_quantized(path: &Path, covs: &[Value]) -> Result<f64, SifisError> {
     Ok(sum / ploc)
 }
 
+// Get the code coverage in percentage
+fn get_coverage_perc(covs: &[Value]) -> Result<f64, SifisError> {
+    let mut tot_lines = 0.;
+    let mut covered_lines = 0.;
+    // count the number of covered lines
+    for i in 0..covs.len() {
+        let is_null = match covs.get(i) {
+            Some(val) => val.is_null(),
+            None => return Err(SifisError::ConversionError()),
+        };
+
+        if !is_null {
+            tot_lines += 1.;
+            let cov = match covs.get(i).unwrap().as_u64() {
+                Some(cov) => cov,
+                None => return Err(SifisError::ConversionError()),
+            };
+            if cov > 0 {
+                covered_lines += 1.;
+            }
+        }
+    }
+    Ok(covered_lines / tot_lines)
+}
+/// Calculate the CRAP value  for the given file(only rust language)
+/// (https://testing.googleblog.com/2011/02/this-code-is-crap.html#:~:text=CRAP%20is%20short%20for%20Change,partner%20in%20crime%20Bob%20Evans.)
+/// Return the value in case of success and an specif error in case of fails
+
+fn crap(path: &Path, covs: &[Value]) -> Result<f64, SifisError> {
+    let data = match read_file(path) {
+        Ok(data) => data,
+        Err(_err) => return Err(SifisError::WrongFile(path.display().to_string())),
+    };
+    let parser = RustParser::new(data, path, None);
+    let root = match metrics(&parser, path) {
+        Some(root) => root,
+        None => return Err(SifisError::MetricsError()),
+    };
+    let comp = root.metrics.cyclomatic.cyclomatic_sum();
+    let cov = get_coverage_perc(covs)?;
+    Ok(((comp.powf(2.)) * ((1.0 - cov).powf(3.))) + comp)
+}
+
+/// Calculate the Skunkscore value  for the given file (only rust language)
+/// https://www.fastruby.io/blog/code-quality/intruducing-skunk-stink-score-calculator.html
+/// In this implementation the code smeels are ignored.
+/// Return the value in case of success and an specif error in case of fails
+
+fn skunk_nosmells(path: &Path, covs: &[Value]) -> Result<f64, SifisError> {
+    let complexity_factor = 25.0;
+    let data = match read_file(path) {
+        Ok(data) => data,
+        Err(_err) => return Err(SifisError::WrongFile(path.display().to_string())),
+    };
+    let parser = RustParser::new(data, path, None);
+    let root = match metrics(&parser, path) {
+        Some(root) => root,
+        None => return Err(SifisError::MetricsError()),
+    };
+    let comp = root.metrics.cyclomatic.cyclomatic_sum();
+    let cov = get_coverage_perc(covs)?;
+    if cov == 1. {
+        Ok(comp / complexity_factor)
+    } else {
+        Ok((comp / complexity_factor) * (100. - (100. * cov)))
+    }
+}
+
 /// This Function get the folder of the repo to analyzed and the path to the json obtained using grcov
-/// It prints all the SIFIS values for all the Rust files in the folders
+/// It prints all the SIFIS, CRAP and SkunkScore values for all the Rust files in the folders
 /// the output will be print as follows:
-/// For "file.rs" the SIFIS plain value is x
-/// For "file.rs" the SIFIS quantized value is x
-pub fn get_sifis<A: AsRef<Path> + Copy, B: AsRef<Path> + Copy>(
+/// FILE       | SIFIS PLAIN | SIFIS QUANTIZED | CRAP       | SKUNKSCORE
+pub fn get_metrics<A: AsRef<Path> + Copy, B: AsRef<Path> + Copy>(
     files_path: A,
     json_path: B,
 ) -> Result<(), SifisError> {
@@ -186,7 +247,7 @@ pub fn get_sifis<A: AsRef<Path> + Copy, B: AsRef<Path> + Copy>(
             ))
         }
     };
-   
+
     let file = match fs::read_to_string(json_path) {
         Ok(file) => file,
         Err(_err) => {
@@ -196,6 +257,11 @@ pub fn get_sifis<A: AsRef<Path> + Copy, B: AsRef<Path> + Copy>(
         }
     };
     let covs = read_json(file, files_path.as_ref().to_str().unwrap())?;
+    //println!("FILE \t SIFIS PLAIN \t SIFIS QUANTIZED \t CRAP \t SKUNKSCORE");
+    println!(
+        "{0: <20} | {1: <20} | {2: <20} | {3: <20} | {4: <20}",
+        "FILE", "SIFIS PLAIN", "SIFIS QUANTIZED", "CRAP", "SKUNKSCORE"
+    );
     for path in vec {
         let arr = match covs.get(&path) {
             Some(arr) => arr.to_vec(),
@@ -203,16 +269,24 @@ pub fn get_sifis<A: AsRef<Path> + Copy, B: AsRef<Path> + Copy>(
         };
         let p = Path::new(&path);
         let sifis = sifis_plain(p, &arr)?;
-        println!(
-            "For {:?} the SIFIS plain value is {:.3?}",
-            p.file_name().unwrap(),
-            sifis
-        );
         let sifis_quantized = sifis_quantized(p, &arr)?;
-        println!(
-            "For {:?} the SIFIS quantized value is {:.3?}",
+        let crap = crap(p, &arr)?;
+        let skunk = skunk_nosmells(p, &arr)?;
+        /*println!(
+            "{:?} \t {:.3} \t {:.3} \t {:.3} \t {:.3}",
             p.file_name().unwrap(),
-            sifis_quantized
+            sifis,
+            sifis_quantized,
+            crap,
+            skunk
+        );*/
+        println!(
+            "{0: <20} | {1: <20.3} | {2: <20.3} | {3: <20.3} | {4: <20.3}",
+            p.file_name().unwrap().to_str().unwrap(),
+            sifis,
+            sifis_quantized,
+            crap,
+            skunk
         );
     }
     Ok(())
@@ -223,30 +297,20 @@ mod tests {
     use std::path::PathBuf;
 
     use super::*;
-    const JSON : &str = "./data/data.json";
-    const PREFIX : &str = "../rust-data-structures-main/";
-    const MAIN : &str = "../rust-data-structures-main/data/main.rs";
-    const SIMPLE : &str = "../rust-data-structures-main/data/simple_main.rs";
-    const FILE : &str =  "./data/simple_main.rs";
+    const JSON: &str = "./data/data.json";
+    const PREFIX: &str = "../rust-data-structures-main/";
+    const MAIN: &str = "../rust-data-structures-main/data/main.rs";
+    const SIMPLE: &str = "../rust-data-structures-main/data/simple_main.rs";
+    const FILE: &str = "./data/simple_main.rs";
     #[test]
     fn test_read_json() {
         let file = fs::read_to_string(JSON).unwrap();
         let covs = read_json(file, PREFIX).unwrap();
-        assert_eq!(
-            covs.contains_key(SIMPLE),
-            true
-        );
-        assert_eq!(
-            covs.contains_key(MAIN),
-            true
-        );
-        let vec = covs
-            .get(SIMPLE)
-            .unwrap();
+        assert_eq!(covs.contains_key(SIMPLE), true);
+        assert_eq!(covs.contains_key(MAIN), true);
+        let vec = covs.get(SIMPLE).unwrap();
         assert_eq!(vec.len(), 12);
-        let vec_main = covs
-            .get(MAIN)
-            .unwrap();
+        let vec_main = covs.get(MAIN).unwrap();
         assert_eq!(vec_main.len(), 9);
         let value = vec.get(6).unwrap();
         assert_eq!(value, 2);
@@ -260,10 +324,7 @@ mod tests {
         let covs = read_json(file, PREFIX).unwrap();
         let mut path = PathBuf::new();
         path.push(FILE);
-        let vec = covs
-            .get(SIMPLE)
-            .unwrap()
-            .to_vec();
+        let vec = covs.get(SIMPLE).unwrap().to_vec();
         let sifis = sifis_plain(&path, &vec).unwrap();
         assert_eq!(sifis, 24. / 10.)
     }
@@ -274,11 +335,30 @@ mod tests {
         let covs = read_json(file, PREFIX).unwrap();
         let mut path = PathBuf::new();
         path.push(FILE);
-        let vec = covs
-            .get(SIMPLE)
-            .unwrap()
-            .to_vec();
+        let vec = covs.get(SIMPLE).unwrap().to_vec();
         let sifis = sifis_quantized(&path, &vec).unwrap();
         assert_eq!(sifis, 6. / 10.)
+    }
+
+    #[test]
+    fn test_crap() {
+        let file = fs::read_to_string(JSON).unwrap();
+        let covs = read_json(file, PREFIX).unwrap();
+        let mut path = PathBuf::new();
+        path.push(FILE);
+        let vec = covs.get(SIMPLE).unwrap().to_vec();
+        let crap = crap(&path, &vec).unwrap();
+        assert_eq!(crap, 5.024)
+    }
+
+    #[test]
+    fn test_skunk() {
+        let file = fs::read_to_string(JSON).unwrap();
+        let covs = read_json(file, PREFIX).unwrap();
+        let mut path = PathBuf::new();
+        path.push(FILE);
+        let vec = covs.get(SIMPLE).unwrap().to_vec();
+        let skunk = skunk_nosmells(&path, &vec).unwrap();
+        assert_eq!(skunk, 6.4)
     }
 }
